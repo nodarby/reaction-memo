e63981fb78250cbda8209ddd39957fb58da2c1ee
nodarby <ndtmhr@outlook.jp>
2019-10-17 10:51:14 +0900

    ファイルのロード機能

--- src/editor.js
+++ src/editor.js
@@ -21,7 +21,50 @@ function onLoad(){
 
     editor = ace.edit('input_txt')
     editor.getSession().setMode('ace/mode/javascript')
-    editor.setTheme('ace/theme/twilight')
+    //editor.setTheme('ace/theme/twilight')
 
+    //今までの頑張りをロード
+    document.querySelector('#btnLoad').addEventListener('click', () => {
+        openLoadFile();
+    })
 
+}
+
+//ファイルを開きます
+function openLoadFile(){
+    const win = BrowserWindow.getFocusedWindow();
+
+    dialog.showOpenDialog(
+        win,
+        //ダイアログの内容を指定
+        {
+            properties: ['openFile'],
+            filters: [
+                {
+                    name:'Documents',
+                    extensions: ['txt', 'text', 'html', 'js']
+                }
+            ]
+        },
+        //閉じた後のコールバック変数
+        (fileNames) => {
+            if (fileNames) {
+                readFile(fileNames[0]);
+            }
+        });
+}
+
+//読み込んだファイルを入力エリアに表示
+function readFile(path) {
+    currentPath = path;
+    fs.readFile(path, (error, text) => {
+        if (error != null) {
+            alert('error : ' + error);
+            return;
+        }
+        //フッター部分に読み込みファイルのパスを設定
+        footerArea.innerHTML = path;
+        //テキスト入力エリアに設定
+        editor.setValue(text.toString(), -1)
+    })
 }
